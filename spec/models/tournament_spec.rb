96143ee2e216689d88d8f28b159199fa2ebbd4af
Catherine Marvin <catherinemmarvin@gmail.com>
2022-09-02 15:03:55 -0700

    Add tests for 3 person case

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @tournament = Tournament.new
+  end
+  context "with 3 people" do
+    before(:each) do
+      players = [ Player.new(name: "A"), Player.new(name: "B"), Player.new(name: "C") ]
+      @tournament.players = players
+      @tournament.generate_rounds
+    end
+
+    it "has a single round" do
+      expect(@tournament.rounds.length).to eq(1)
+    end
+
+    it "gives each player the same round" do
+      round = @tournament.rounds.first
+      expect(round.players).to eq(@tournament.players)
+    end
+  end
 end
