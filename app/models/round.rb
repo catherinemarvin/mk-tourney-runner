a0016c7c656cd327248e9ecf0c0421a1d1149cc8
Catherine Marvin <catherinemmarvin@gmail.com>
2022-09-19 16:59:22 -0700

    Fix issue where records could not be saved.
    inverse_of is required to ensure that RoundEdges got populated correctly

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,9 +2,9 @@ class Round < ApplicationRecord
     belongs_to :tournament
     has_and_belongs_to_many :players
 
-    has_many :previous_round_edges, foreign_key: :end_id, class_name: "RoundEdge"
-    has_many :previous_rounds, through: :previous_round_edges, source: :start
+    has_many :previous_round_edges, foreign_key: :end_id, class_name: "RoundEdge", inverse_of: :end
+    has_many :previous_rounds, through: :previous_round_edges, source: :start, inverse_of: :next_rounds
 
-    has_many :next_round_edges, foreign_key: :start_id, class_name: "RoundEdge"
-    has_many :next_rounds, through: :next_round_edges, source: :end
+    has_many :next_round_edges, foreign_key: :start_id, class_name: "RoundEdge", inverse_of: :start
+    has_many :next_rounds, through: :next_round_edges, source: :end, inverse_of: :previous_rounds
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -19,6 +19,9 @@ class Tournament < ApplicationRecord
         num_full_rounds = num_players / 4
         num_leftover = num_players % 4
 
+        # Tracks the number of rounds to create
+        round_position = 0
+
         if num_full_rounds > 4
             # TODO(catherine): Do recursion.
             # round_1_winners = num_full_rounds
@@ -30,10 +33,11 @@ class Tournament < ApplicationRecord
         if num_leftover == 0
             final_round = rounds.build()
             players.each_slice(4) do |player_chunk|
-                round = rounds.build(players: player_chunk)
+                round = rounds.build(players: player_chunk, position: round_position)
+                round_position += 1
                 round.next_rounds << final_round
-                final_round.previous_rounds << round
             end
+            final_round.position = round_position
             # TODO(catherine): Add functionality to enable multiple previous/next rounds
             # final_round = rounds.build()
             # extra_round = rounds.build(next_round = final_round)
